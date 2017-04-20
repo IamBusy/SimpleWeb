0910b2572910d7f18749c10b179b4e04f7dccf83
william wei <1342247033@qq.com>
2017-04-20 10:50:03 +0800

    fix bug of overflow

--- core/router.go
+++ core/router.go
@@ -6,12 +6,25 @@ import (
 )
 
 
+/**
+ * Router
+ */
 type Router struct {
 	router *httprouter.Router
-
 }
 
-func (router Router)handle(method string, uri string, middlewares []MiddleWare, handler httprouter.Handle)  {
+/**
+ * the entry for applying middleware and handling request
+ */
+func (router *Router)handle(method string, uri string, middlewares []MiddleWare, handler httprouter.Handle)  {
+
+	if middlewares == nil {
+		middlewares = make([]MiddleWare,0)
+	}
+
+	if router.router == nil {
+		router.router = httprouter.New()
+	}
 
 	router.router.Handle(method, uri, func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		for i:=0;i<len(middlewares);i++ {
@@ -20,24 +33,28 @@ func (router Router)handle(method string, uri string, middlewares []MiddleWare, 
 
 		handler(w,r,ps)
 
-		for i:=len(middlewares);i>=0;i++ {
-			middlewares[i].out(w,r,ps)
+		for i:=len(middlewares);i>=1;i-- {
+			middlewares[i-1].out(w,r,ps)
 		}
 	})
 }
 
-func (router Router) GET(uri string,middlewares []MiddleWare ,handler httprouter.Handle)  {
+func (router *Router) GET(uri string,middlewares []MiddleWare ,handler httprouter.Handle)  {
 	router.handle("GET",uri,middlewares,handler)
 }
 
-func (router Router) POST(uri string,middlewares []MiddleWare ,handler httprouter.Handle)  {
+func (router *Router) POST(uri string,middlewares []MiddleWare ,handler httprouter.Handle)  {
 	router.handle("POST",uri,middlewares,handler)
 }
 
-func (router Router) PUT(uri string,middlewares []MiddleWare ,handler httprouter.Handle)  {
+func (router *Router) PUT(uri string,middlewares []MiddleWare ,handler httprouter.Handle)  {
 	router.handle("PUT",uri,middlewares,handler)
 }
 
-func (router Router) DELETE(uri string,middlewares []MiddleWare ,handler httprouter.Handle)  {
+func (router *Router) DELETE(uri string,middlewares []MiddleWare ,handler httprouter.Handle)  {
 	router.handle("DELETE",uri,middlewares,handler)
+}
+
+func (router *Router) ServeHTTP(w http.ResponseWriter, req *http.Request)  {
+	router.router.ServeHTTP(w,req)
 }
