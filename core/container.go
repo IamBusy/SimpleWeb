e7e169e63ba5ba0b548d163e6ec43e094a852045
william wei <1342247033@qq.com>
2017-04-22 16:57:31 +0800

    support for bootstrapContainer

--- core/container.go
+++ core/container.go
@@ -1,17 +1,30 @@
 package core
 
 type Container interface {
+	Bind(name string,buildFunc func(Container) interface{})
+	Put(name string, inst interface{})
+	Get(name string) (interface{},bool)
+
 	RegisterService(name string, service Service)
-	Service(name string) Service
+	Service(name string) *Service
 	RegisterMiddleWare(name string, middleware MiddleWare)
 	Middleware(name string) (*MiddleWare)
+	RegisterDB(db Manager)
+	DB() Manager
+
+
+	AfterBootstrap(callback func(Container))
+	Bootstrap()()
 }
 
 type container struct {
-	//items map[string]func() struct{}
-	//instances map[string]struct{}
+	items map[string]func(Container) interface{}
+	instances map[string]interface{}
 	services map[string]*Service
 	middlewares map[string]*MiddleWare
+	db Manager
+	bootstrapFunc []func(Container)
+
 }
 
 /**
@@ -21,30 +34,33 @@ func NewContainer() Container  {
 	ctn := &container{}
 	ctn.services = make(map[string]*Service)
 	ctn.middlewares = make(map[string]*MiddleWare)
+	ctn.bootstrapFunc = make([]func(Container),0)
+	ctn.items = make(map[string]func(Container) interface{})
+	ctn.instances = make(map[string] interface{})
 	return ctn
 }
 
+func (ctn *container) Bind(name string,buildFunc func(Container) interface{})  {
+	ctn.items[name] = buildFunc;
+}
 
+func (ctn *container) Put(name string, inst interface{})  {
+	ctn.instances[name] = inst
+}
 
-
-
-//func (ctn *container) Bind(name string,buildFunc func() struct{})  {
-//	ctn.items[name] = buildFunc;
-//}
-//
-//func (ctn *container) Build(name string) (struct{},bool) {
-//	instance, exist := ctn.instances[name]
-//	if exist {
-//		return instance,true
-//	} else {
-//		if buildFunc,exist := ctn.items[name];exist {
-//			ctn.instances[name] = buildFunc()
-//			return ctn.instances[name],true
-//		} else {
-//			return struct {}{},false
-//		}
-//	}
-//}
+func (ctn *container) Get(name string) (interface{},bool) {
+	instance, exist := ctn.instances[name]
+	if exist {
+		return instance,true
+	} else {
+		if buildFunc,exist := ctn.items[name];exist {
+			ctn.instances[name] = buildFunc(ctn)
+			return ctn.instances[name],true
+		} else {
+			return nil,false
+		}
+	}
+}
 
 /**
  * Register Service in the Container
@@ -56,7 +72,7 @@ func (ctn *container) RegisterService(name string, service Service)  {
 /**
  * Get a Service in the Container
  */
-func (ctn *container) Service(name string) (Service) {
+func (ctn *container) Service(name string) (*Service) {
 	service,exist := ctn.services[name]
 	if !exist {
 		return nil
@@ -80,4 +96,29 @@ func (ctn *container) Middleware(name string) (*MiddleWare) {
 		return nil
 	}
 	return middleware
+}
+
+func (ctn *container) RegisterDB(db Manager)  {
+	ctn.db = db
+}
+
+func (ctn *container) DB() Manager  {
+	return ctn.db
+}
+
+
+/**
+ * Add callback function when bootstrapping
+ */
+func (ctn *container) AfterBootstrap(callback func(Container))  {
+	ctn.bootstrapFunc = append(ctn.bootstrapFunc,callback)
+}
+
+/**
+ * Bootstrap the container
+ */
+func (ctn *container) Bootstrap()  {
+	for i:=0; i<len(ctn.bootstrapFunc); i++ {
+		ctn.bootstrapFunc[i](ctn)
+	}
 }
